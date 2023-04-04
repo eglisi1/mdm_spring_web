81befa65ceb47523b4667131c4da16f895c42db8
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-04 14:52:42 +0200

    init commit after spring initializr

---
+++ src/test/java/ch/eglisi1/springweb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.eglisi1.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
