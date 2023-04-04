b88d648458b5dc1b4fa3f52974f443f023919e47
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-04 15:35:04 +0200

    add logger example

--- src/main/java/ch/eglisi1/springweb/rest/HelloController.java
+++ src/main/java/ch/eglisi1/springweb/rest/HelloController.java
@@ -1,13 +1,18 @@
 package ch.eglisi1.springweb.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        logger.info("sayHello() called with name: {}", name);
         return String.format("Hello %s!", name);
     }
 }
